7ed148e76b03dfe3f524fa96f297c267451a39a6
andy <andy@hxr.us>
2018-10-02 06:58:22 -0400

    a couple of changes

--- src/main/kotlin/net/arin/bootstrap_fetcher/HelloWorld.kt
+++ src/main/kotlin/net/arin/bootstrap_fetcher/HelloWorld.kt
@@ -5,10 +5,9 @@ import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@WebServlet(name = "Hello", value = "/")
+@WebServlet(name = "Hello", value = ["/hello"])
 class HelloWorld : HttpServlet() {
     override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
         res.writer.write("Hello, World! I am a Servlet 3.1 running on Java8 App Engine Standard, and written in Kotlin...")
-
     }
 }
