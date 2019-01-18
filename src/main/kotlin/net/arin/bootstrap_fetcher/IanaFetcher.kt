8256c68007b7dcc6c4aae69e841ceb9260ded8c3
andy <andy@arin.net>
2019-01-18 07:40:51 -0800

    latest

--- src/main/kotlin/net/arin/bootstrap_fetcher/IanaFetcher.kt
+++ src/main/kotlin/net/arin/bootstrap_fetcher/IanaFetcher.kt
@@ -16,13 +16,8 @@
  */
 package net.arin.bootstrap_fetcher
 
-import com.google.cloud.storage.Blob
-import com.google.cloud.storage.BlobId
-import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.StorageOptions
 import java.net.URL
-import java.nio.channels.Channels
-import java.time.LocalDateTime
 
 
 private val refreshHeader = "x-refresh"
@@ -32,33 +27,15 @@ private val refreshHeader = "x-refresh"
  */
 fun fetchIana( ianaUrl: String, gcsName : String ) {
 
-    val bucket : String? = System.getProperty( BUCKET_NAME_PROPERTY )
+    val bucketName : String? = System.getProperty( BUCKET_NAME_PROPERTY )
 
-    bucket?.let{
+    bucketName?.let{
 
         val url = URL( ianaUrl )
 
         val storage = StorageOptions.getDefaultInstance().service
-        val blobId = BlobId.of( bucket, gcsName )
-        val blob : Blob? = storage.get( blobId )
-        blob?.let {
-            val writer = blob.writer()
-            val os = Channels.newOutputStream( writer )
-            url.openStream().copyTo( os )
-            blob.toBuilder().setMetadata( hashMapOf( refreshHeader to LocalDateTime.now().toString() ) ).build().update()
-        } ?: run {
-            storage.create(
-                    BlobInfo
-                            .newBuilder( bucket, gcsName )
-                            .setMetadata(
-                                    hashMapOf(
-                                            refreshHeader to LocalDateTime.now().toString() )
-                            )
-                            .setContentType( "application/json" )
-                            .build(),
-                    url.openStream()
-            )
-        }
+        val bucket = storage.get( bucketName )
+        bucket.create( gcsName, url.openStream(), "application/json" )
 
     } ?: run {
         throw RuntimeException( "environment variable ${BUCKET_NAME_PROPERTY} not set" )
